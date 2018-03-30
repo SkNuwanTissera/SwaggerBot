c59bcbe9fcd419394a8520df8850a81756fd6193
Nuwan Tissera <nuwan.tissera@infor.com>
2018-03-30 17:24:22 +0530

    Swagger

--- src/main/java/SwaggerParserTest.java
+++ src/main/java/SwaggerParserTest.java
@@ -1,24 +1,29 @@
 /**
  * Created by Sk
  */
+
 import io.swagger.parser.SwaggerParser;
+import io.swagger.util.Json;
 import io.swagger.models.Swagger;
 
-public class SwaggerParser {
+public class SwaggerParserTest {
 
     public static void main(String [] args){
         Swagger parser = new SwaggerParser().read("http://petstore.swagger.io/v2/swagger.json");
 
         //You can read from a file location as well:
-        //Swagger swagger = new SwaggerParser().read("./path/to/swagger.json");
+        //Swagger swagger = new SwaggerParserTest().read("./path/to/swagger.json");
 
         //swagger-compat-spec-parser module, you can read older formats, and convert them into swagger 2.0:
         //Swagger swagger = new SwaggerCompatConverter().read("http://petstore.swagger.io/api/api-docs");
 
-        String swaggerString = Json.pretty(swagger);
+        String swaggerString = Json.pretty(parser);
+        System.out.println(swaggerString);
 
 
 
     }
+
+
 
 }
